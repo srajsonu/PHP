93b6c352e165df319bb4f61645cd2055107eedcf
srajsonu <43319626+srajsonu@users.noreply.github.com>
2019-03-07 20:50:00 +0530

    linked with php

---
+++ captain.php
@@ -0,0 +1,17 @@
+ <?php
+ 	$u=$_POST['username'];
+ 	$e=$_POST['Email'];
+ 	$p=$_POST['Password'];
+  $r=$_POST['Re-enter'];
+
+
+ 	$a = mysqli_connect('192.168.64.2','root','','Alexa');
+
+  if ($p==$r) {
+    $ins="INSERT INTO A1 SET username='$u',Email='$e',Password='$p'";
+    $a->query($ins);
+  }else {
+    echo "Password doesn't Match";
+  }
+
+ ?>
